d75dae2fa42f08dcfe054bd0d91050463ed1713f
kusht <kushal.tirumala@rubrik.com>
2018-09-16 04:00:18 -0700

    working calendar

--- js/collegeBar.js
+++ js/collegeBar.js
@@ -20,122 +20,22 @@ var div2Style = {
     textAlign: 'center'
 };
 
-const events = [
-    {
-        id: 0,
-        title: 'All Day Event very long title',
-        allDay: true,
-        start: new Date(2015, 3, 0),
-        end: new Date(2015, 3, 1),
-    },
-    {
-        id: 1,
-        title: 'Long Event',
-        start: new Date(2015, 3, 7),
-        end: new Date(2015, 3, 10),
-    },
-
-    {
-        id: 2,
-        title: 'DTS STARTS',
-        start: new Date(2016, 2, 13, 0, 0, 0),
-        end: new Date(2016, 2, 20, 0, 0, 0),
-    },
-
-    {
-        id: 3,
-        title: 'DTS ENDS',
-        start: new Date(2016, 10, 6, 0, 0, 0),
-        end: new Date(2016, 10, 13, 0, 0, 0),
-    },
-
-    {
-        id: 4,
-        title: 'Some Event',
-        start: new Date(2015, 3, 9, 0, 0, 0),
-        end: new Date(2015, 3, 10, 0, 0, 0),
-    },
-    {
-        id: 5,
-        title: 'Conference',
-        start: new Date(2015, 3, 11),
-        end: new Date(2015, 3, 13),
-        desc: 'Big conference for important people',
-    },
-    {
-        id: 6,
-        title: 'Meeting',
-        start: new Date(2015, 3, 12, 10, 30, 0, 0),
-        end: new Date(2015, 3, 12, 12, 30, 0, 0),
-        desc: 'Pre-meeting meeting, to prepare for the meeting',
-    },
-    {
-        id: 7,
-        title: 'Lunch',
-        start: new Date(2015, 3, 12, 12, 0, 0, 0),
-        end: new Date(2015, 3, 12, 13, 0, 0, 0),
-        desc: 'Power lunch',
-    },
-    {
-        id: 8,
-        title: 'Meeting',
-        start: new Date(2015, 3, 12, 14, 0, 0, 0),
-        end: new Date(2015, 3, 12, 15, 0, 0, 0),
-    },
-    {
-        id: 9,
-        title: 'Happy Hour',
-        start: new Date(2015, 3, 12, 17, 0, 0, 0),
-        end: new Date(2015, 3, 12, 17, 30, 0, 0),
-        desc: 'Most important meal of the day',
-    },
-    {
-        id: 10,
-        title: 'Dinner',
-        start: new Date(2015, 3, 12, 20, 0, 0, 0),
-        end: new Date(2015, 3, 12, 21, 0, 0, 0),
-    },
-    {
-        id: 11,
-        title: 'Birthday Party',
-        start: new Date(2015, 3, 13, 7, 0, 0),
-        end: new Date(2015, 3, 13, 10, 30, 0),
-    },
-    {
-        id: 12,
-        title: 'Late Night Event',
-        start: new Date(2015, 3, 17, 19, 30, 0),
-        end: new Date(2015, 3, 18, 2, 0, 0),
-    },
-    {
-        id: 12.5,
-        title: 'Late Same Night Event',
-        start: new Date(2015, 3, 17, 19, 30, 0),
-        end: new Date(2015, 3, 17, 23, 30, 0),
-    },
-    {
-        id: 13,
-        title: 'Multi-day Event',
-        start: new Date(2015, 3, 20, 19, 30, 0),
-        end: new Date(2015, 3, 22, 2, 0, 0),
-    },
-    {
-        id: 14,
-        title: 'Today',
-        start: new Date(new Date().setHours(new Date().getHours() - 3)),
-        end: new Date(new Date().setHours(new Date().getHours() + 3)),
-    },
-];
-
-
 class CollegeBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             colleges: [],
             currentCollege: "UCLA",
-            events: []
-        }
+            events: [ {
+                id: 0,
+                title: 'hi',
+                allDay: true,
+                start: new Date(2018, 9, 1),
+                end: new Date(2018, 9, 2)
+            }]
+        };
+
+        this.handleOnChange = this.handleOnChange.bind(this);
 
     };
 
@@ -157,13 +57,36 @@ class CollegeBar extends React.Component {
 
 
 
-    handleOnChange () {
+    handleOnChange (event) {
+        var temp = [];
+        for (var i = 0; i < event.length; i++) {
+            var collegeName = event[i].value;
+            console.log("pushing " + collegeName);
+            var tempEvent =    {
+                    id: i,
+                      title: collegeName,
+                        allDay: true,
+                       start: new Date(2018, 9, 5),
+                       end: new Date(2018, 9, 6)
+            };
+
+            temp.push(tempEvent);
+        }
+        console.log(event);
+        console.log(typeof(event));
         console.log("changing college state");
+
+        this.setState({events: temp});
     };
 
     handleSelect(event) {
         console.log(event);
     }
+
+
+    // createEvents() {
+    //
+    // }
 
     render() {
 
@@ -184,9 +107,9 @@ class CollegeBar extends React.Component {
                 <div style={divStyle}>
                 <BigCalendar
                     selectable
-                    events={events}
-                    startAccessor='startDate'
-                    endAccessor='endDate'
+                    events={this.state.events}
+                    startAccessor='start'
+                    endAccessor='end'
                     onSelectEvent={event => alert(event.title)}
                     onSelectSlot={this.handleSelect}
                 />
